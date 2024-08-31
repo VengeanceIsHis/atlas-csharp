05c3abba00482cd559929c076c742d61ff236822
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-08-31 18:27:45 -0500

    Test for task 4

---
+++ csharp-ifelse_loops_method/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 99; i++)
+        {
+
+            Console.WriteLine(i + " " + "=" + " " + i.ToString("X"));
+        }
+    }
+}
