a38d62fcbde5375a9f5527d7765e620be3800db6
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-08-31 22:05:07 -0500

    Test for 6

---
+++ csharp-ifelse_loops_method/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = i + 1; j < 10; j++)
+            {
+                Console.WriteLine($"{i} {j}");
+            }
+        }
+    }
+}
