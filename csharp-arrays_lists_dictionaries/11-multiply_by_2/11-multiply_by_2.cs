f2dc8719524f66260f59bba80360fde83d8b6397
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-09-03 11:17:40 -0500

    Finished task 11

--- csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
+++ csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -6,6 +6,12 @@ class Dictionary
 {
     public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
     {
-        
+        var keys = new List<string>(myDict.Keys);
+
+        foreach (var key in keys)
+        {
+            myDict[key] *= 2;
+        }
+        return myDict;
     }
 }
