0f4e87952f5d5e363168689ee1827007791a753a
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-09-02 20:09:59 -0500

    Pushing unfinished 11

---
+++ csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        
+    }
+}
