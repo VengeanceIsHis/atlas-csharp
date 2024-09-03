d4429a91fa5bb2d77d2127ae290b818002d739c1
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-09-03 17:57:45 -0500

    test

--- csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -4,17 +4,17 @@ class Program
 {
     static void Main()
     {
-       
-        string output = "";
-
         for (int i = 0; i < 100; i++)
         {
-          
-            string formattedNumber = i < 10 ? $"0{i}" : i.ToString();
-            
-            output += formattedNumber + (i < 99 ? ", " : "\n");
+           
+            if (i < 99)
+            {
+                Console.Write("{0:D2}, ", i);
+            }
+            else
+            {
+                Console.WriteLine("{0:D2}", i);
+            }
         }
-        
-        Console.Write(output);
     }
 }
