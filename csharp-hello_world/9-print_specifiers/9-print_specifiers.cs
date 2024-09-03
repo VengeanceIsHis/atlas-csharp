31f1a4f8dd85032bf7cff72835fda9b823154d2e
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-09-03 17:20:27 -0500

    Test for task 9

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -6,7 +6,7 @@ class Program
 	{
 		double percent = .7553;
 		double currency = 98765.4321;
-		Console.WriteLine("{0:P2}", percent);
-        Console.WriteLine("{0:F2}", currency);
+		Console.WriteLine("Percent: {0:P2}", percent);
+        Console.WriteLine("Currency: {0:F2}", currency);
 	}
 }
