be5588460e7e51a45a9949ca90ee034375a8e9b9
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-09-03 20:56:27 -0500

    test

--- csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -5,18 +5,6 @@ class Array
 {
     public static int elementAt(int[] array, int index)
     {
-        for (int i = 0; i < array.Length; i++)
-        {
-            if (index > array.Length || index < 0)
-            {
-                Console.WriteLine("Index out of range");
-                return -1;
-            }
-            if (i == index)
-            {
-                return array[i];
-            }
-        }
-        return -1;
+        return array[index];
     }
 }
