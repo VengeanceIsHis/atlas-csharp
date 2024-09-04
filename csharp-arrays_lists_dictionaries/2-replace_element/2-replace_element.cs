62f063ca2fd69356b242a5d961cd11c70aab0207
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-09-03 21:09:08 -0500

    test 2

--- csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -5,16 +5,15 @@ class Array
 {
     public static int[] ReplaceElement(int[] array, int index, int n)
     {
-         if (index < 0 || index >= array.Length)
+         if (index < 0 || index >= array.Length - 1)
         {
             Console.WriteLine("Index out of range");
             return array; 
         }
-
-        int[] newArray = (int[])array.Clone();
-
-        newArray[index] = n;
-
-        return newArray;
+        else
+        {
+            array[index] = n;
+            return array;
+        }
     }
 }
