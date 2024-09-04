06947dae7ec8aa14daee9e2fdb8447dd1f38dd8b
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-09-03 22:02:50 -0500

    Test 4

--- csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -11,6 +11,10 @@ class List
         for (int i = 0; i < size; i++)
         {
             temp.Add(i);
+            if (i == size - 1)
+            {
+                Console.Write(i);
+            }
             Console.Write(i + " ");
         }
         Console.WriteLine();
