3e55ba9dd43f1c4cb9d9515a78409c9f3987f693
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-09-05 18:48:38 -0500

    Testing 9

--- csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -7,6 +7,8 @@ class LList
     public static int Pop(LinkedList<int> myLList)
     {
         LinkedListNode<int> current = myLList.First;
+        int value = current.Value;
         myLList.Remove(current);
     }
+    return value;
 }
