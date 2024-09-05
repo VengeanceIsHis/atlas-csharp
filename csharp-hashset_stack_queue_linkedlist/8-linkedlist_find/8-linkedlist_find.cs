442e7491f8e172e489ccb260fa490d7d15794bd4
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-09-05 18:21:12 -0500

    Adding task 8

---
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        LinkedListNode<int> current = linkedList.First;
+        int position = 0;
+        foreach (int obj in myLList)
+        {
+            if (current.Value == value)
+            {
+                return position;
+            }
+            current = current.Next;
+            position++;
+        }
+        return -1;
+    }
+}
