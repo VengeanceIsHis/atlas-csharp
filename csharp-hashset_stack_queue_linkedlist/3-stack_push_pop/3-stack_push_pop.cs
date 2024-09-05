fc5270a2be830b3980c3687faf911fda24c69a50
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-09-05 10:44:14 -0500

    Test

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+
+class Mystack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: " + aStack.Count)
+    }
+}
