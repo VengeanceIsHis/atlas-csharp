7f640d1ceb10b294ba88f83cf024727212cf06e9
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-09-05 10:45:57 -0500

    Push

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -6,6 +6,12 @@ class Mystack
 {
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
-        Console.WriteLine("Number of items: " + aStack.Count)
+        Console.WriteLine("Number of items: " + aStack.Count);
+        if (aStack.Count == 0)
+        {
+            Console.WriteLine("Stack is empty")
+        }
+        Console.WriteLine("Top item: " + aStack[aStack.Count]);
+
     }
 }
