dcdd6ceb2a75e40ba1fa746b72f48199c8d56181
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-09-05 17:35:49 -0500

    Adding task 6

---
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int count;
+        foreach (int obj in myLList)
+        {
+            count += 1;
+        }
+        return count;
+    }
+}
