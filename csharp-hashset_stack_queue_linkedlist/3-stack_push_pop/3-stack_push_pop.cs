fd691b0a117e1019a8d45d8b1d1c8d1144ffb756
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-09-05 11:14:19 -0500

    Test for task 3

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -6,12 +6,26 @@ class Mystack
 {
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
+        Stack<int> tempStack = new Stack<int>();
+
         Console.WriteLine("Number of items: " + aStack.Count);
-        if (aStack.Count == 0)
+        if (aStack.isEmpty)
         {
             Console.WriteLine("Stack is empty")
         }
-        Console.WriteLine("Top item: " + aStack[aStack.Count]);
+        Console.WriteLine("Top item: " + aStack.peek());
 
+        if (aStack.Contains("search"))
+        {
+            while(aStack.Count > 0);
+            int item = aStack.Pop();
+            if (item == "search")
+            {
+                break;
+            }
+        }
+        Astack.push(newItem);
+
+        return Astack;
     }
 }
