5f5a405caa459fea52d88828a00f02a539f271ea
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-09-05 10:30:00 -0500

    test

--- csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -9,13 +9,24 @@ class List
         List<int> temp = new List<int>();
         for (int i = 0; i < list1.Count; i++)
         {
-            if (list2.Contains(list1[i]) || list1.Contains(list2[i]))
+            if (list2.Contains(list1[i]))
             {
                 continue;
             }
             else
             {
                 temp.Add(list1[i]);
+            }
+        }
+        for (int i = 0; i < list2.Count; i++)
+        {
+            if (list1.Contains(list2[i]))
+            {
+                continue;
+            }
+            else
+            {
+                temp.Add(list2[i]);
             }
         }
         return temp;
