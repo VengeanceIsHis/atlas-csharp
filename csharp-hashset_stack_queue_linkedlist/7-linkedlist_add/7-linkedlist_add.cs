32f4da97081153e3fed5de273d8c3d15d83ae37a
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-09-05 17:43:01 -0500

    adding to first

--- csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
+++ csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -6,7 +6,7 @@ class LList
 {
     public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
     {
-        LinkedListNode<int> node = myLList.AddLast(n);
+        LinkedListNode<int> node = myLList.AddFirst(n);
         return node;
     }
 }
