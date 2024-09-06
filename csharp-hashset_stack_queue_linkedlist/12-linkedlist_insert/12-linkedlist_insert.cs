420acbc2447d9f0334eb86c64de4b048f1ff006d
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-09-06 10:26:49 -0500

    Adding new directory

---
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+
+class LList
+{
+    public static LinkedListNode<int> Insert(linkedList<int> myLList, int n)
+    {
+        int i = 0;
+        LinkedListNode<int> current = myLList.First;
+        LinkedListNode<int> new = new LinkedListNode<int>;
+        new.Value = n;
+        while (current != null)
+        {
+            if (current.Value < n)
+            {
+                if current.Prev == null
+            }
+            i++;
+            current = current.Next;
+        }
+    }
+}
