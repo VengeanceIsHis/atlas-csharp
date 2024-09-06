b2e549dcc96b768f3d2b09c016518108f497b35d
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-09-05 19:29:23 -0500

    Adding task 10

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int i = 0;
+        LinkedListNode<int> current = myLList.First;
+        while (current.Next != null)
+        {
+            if (i == n)
+            {
+                return current.Value;
+            }
+            current = current.Next;
+        }
+        return 0;
+    }
+}
