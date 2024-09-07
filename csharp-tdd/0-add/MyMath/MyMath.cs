e783dccfd9aec8ed1311701ea5329da0badd4a58
VengeanceIsHis <elijahjpoole@gmail.com>
2024-09-07 16:36:44 -0500

    Test

--- csharp-tdd/0-add/MyMath/MyMath.cs
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -1,6 +1,14 @@
-﻿namespace MyMath;
-
-public class Class1
+﻿namespace MyMath
 {
-
+    class Operations
+    {
+        public static int Add(int a, int b)
+        {
+            /// <summary>
+            /// simply returns two integers
+            /// </summary>
+            return a + b;
+        }
+    }
 }
+
