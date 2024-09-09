89143718607513e828be2e5ec6bb8eae1b22b67d
VengeanceIsHis <elijahjpoole@gmail.com>
2024-09-09 18:43:02 -0500

    Adding task 1

---
+++ csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -0,0 +1,26 @@
+﻿namespace MyMath
+{
+
+
+public class Matrix
+{
+    public static int[,] Divide(int[,] matrix, int num)
+    {
+        int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            int[,] result = new int[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    result[i, j] = matrix[i, j] / num;
+                }
+            }
+
+            return result;
+    }
+
+}
+}
