5f6bf3772f95cf8054ee1d7ad626462d301355ea
VengeanceIsHis <elijahjpoole@gmail.com>
2024-09-12 12:57:29 -0500

    Test for task 13

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> current = myLList.First;
+        LinkedListNode<int> result = null;
+
+        for (int i = 0; i < myLList.Count; i++)
+        {
+            if (i == index)
+            {
+                myLList.Remove(current);
+            }
+        }
+    }
+}
