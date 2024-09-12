b5e4241dcc3611273b2379c79fc1b0990937d81e
VengeanceIsHis <elijahjpoole@gmail.com>
2024-09-12 13:22:49 -0500

    TEst

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+
+class List
+
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int result = 0;
+        for (int i = 0; i < n; i++)
+        {
+            try
+            {
+                Console.Write(myList[i]);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.Write("Error index out of range");
+            }
+            result++;
+        }
+    }
+}
