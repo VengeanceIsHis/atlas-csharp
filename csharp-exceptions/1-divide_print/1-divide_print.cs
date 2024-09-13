30fc72dd82ed90a3a694ba69123a1c836e274d6e
VengeanceIsHis <elijahjpoole@gmail.com>
2024-09-12 19:23:06 -0500

    Test

---
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,21 @@
+using System;
+
+
+class int
+{
+    public static void divide(int a, int b)
+    {
+        try
+        {
+            int result = a / b;
+        }
+        catch
+        {
+            return;
+        }
+        finally
+        {
+            return result;
+        }
+    }
+}
