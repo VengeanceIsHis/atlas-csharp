4ffac1400776533e6bc5356a2ac34545512e077c
VengeanceIsHis <elijahjpoole@gmail.com>
2024-09-13 11:37:29 -0500

    Adding more infor for struct

--- csharp-structs_enums/1-dog/1-dog.cs
+++ csharp-structs_enums/1-dog/1-dog.cs
@@ -16,4 +16,14 @@ public struct Dog
     public string owner { get; set; }
     public Rating rating { get; set; }
 
+    public Dog(string name, float age, string owner, Rating rating)
+    {
+        Name = name;
+        Age = age;
+        Owner = owner;
+        Rating = rating;
+    }
+
+    
+
 }
