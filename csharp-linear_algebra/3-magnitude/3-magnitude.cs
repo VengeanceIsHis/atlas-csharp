7713870446a807940d9d33fc88c2108d76f6d87f
VengeanceIsHis <elijahjpoole@gmail.com>
2024-09-13 13:32:25 -0500

    test

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -5,6 +5,10 @@ class VectorMath
 {
     public static double Magnitude(double[] vector)
     {
+        if (vector.length < 2 || vector.length > 3)
+        {
+            return -1;
+        }
         double sum = 0;
 
         foreach (double component in vector)
