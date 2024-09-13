c96f90aeca6488d58b84a08c4434384b4879ca33
VengeanceIsHis <elijahjpoole@gmail.com>
2024-09-13 12:19:19 -0500

    adding xml comments

--- csharp-classes/0-enemy/0-enemy.cs
+++ csharp-classes/0-enemy/0-enemy.cs
@@ -2,7 +2,14 @@ using System;
 
 namespace Enemies
 {
+    /// <summary>
+    /// Enemy details.
+    /// </summary>
     public class Zombie
     {
+        /// <summary>
+        /// Zombie specific data.
+        /// </summary>
+
     }
 }
