e934485174ef5b77f977d2a6ef15c21886a32161
VengeanceIsHis <elijahjpoole@gmail.com>
2024-09-13 11:44:47 -0500

    TEST

--- csharp-structs_enums/1-dog/1-dog.cs
+++ csharp-structs_enums/1-dog/1-dog.cs
@@ -10,19 +10,19 @@ public enum Rating
 
 
 public struct Dog
-{
-    public string Name { get; set; }
-    public float Age { get; set; }
-    public string Owner { get; set; }
-    public Rating Rating { get; set; }
-
-    public Dog(string name, float age, string owner, Rating rating)
     {
-        Name = name;
-        Age = age;
-        Owner = owner;
-        Rating = rating;
-    }
+        public string Name { get; set; }
+        public float Age { get; set; }
+        public string Owner { get; set; }
+        public Rating Rating { get; set; }
+
+        public Dog(string name, float age, string owner, Rating rating)
+        {
+            Name = name;
+            Age = age;
+            Owner = owner;
+            Rating = rating;
+        }
 
 
 
