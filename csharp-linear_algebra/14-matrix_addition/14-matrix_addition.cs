1ed187c3215b11fae955b75cbc954af886f9929a
VengeanceIsHis <elijahjpoole@gmail.com>
2024-09-14 12:11:00 -0500

    Test

--- csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -10,13 +10,16 @@ class MatrixMath
         int rows2 = matrix2.GetLength(0);
         int cols2 = matrix2.GetLength(1);
         
-        double[,] result = new double[vector.Length]; 
+         
         if (rows1 != rows2 || cols1 != cols2 || rows1 < 2 || rows1 > 3)
         {
-            result[0, 0] = -1;
+            double[,] check = new double[rows1, cols1];
+            check[0, 0] = -1;
+            return check;
         }
         else
         {
+            double[,] result = new double[rows1, cols1];
             for (int i = 0; i < rows1; i++)
             {
                 for (int j = 0; j < cols1; j++)
@@ -24,7 +27,8 @@ class MatrixMath
                     result[i, j] = matrix1[i, j] + matrix2[i, j];
                 }
             }
+            return result;
         }
-        return result;
+        
     }
 }
