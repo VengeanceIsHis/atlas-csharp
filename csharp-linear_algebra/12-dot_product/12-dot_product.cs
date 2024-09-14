507fe4369297085a83e5032d0e6b00d94e461f3c
VengeanceIsHis <elijahjpoole@gmail.com>
2024-09-14 11:55:08 -0500

    Test

--- csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -5,18 +5,18 @@ class VectorMath
 {
     public static double DotProduct(double[] vector1, double[] vector2)
     {
-        double[] result = new double[vector1.Length];
+        double result = 0;
         if (vector1.Length != vector2.Length || vector1.Length < 2 || vector1.Length > 3)
         {
-            result[0] = -1;
+            result = -1;
         }
 
         else
         {
-        for (int i = 0; i < vector1.Length; i++)
-        {
-            result[i] = Math.Round(vector1[i] * vector2[i]);
-        }
+            for (int i = 0; i < vector1.Length; i++)
+            {
+                result += Math.Round(vector1[i] * vector2[i]);
+            }
         }
 
         return result;
