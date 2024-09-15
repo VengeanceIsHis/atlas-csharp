cd4a4046f0f301008b9331a34cfee9707b0bc865
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-09-15 15:21:49 -0500

    Name change

--- csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -1,31 +1,26 @@
 using System;
 using System.Collections.Generic;
-using System.Xml.XPath;
 
 
 class LList
 {
-    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    public static LinkedListNode<int> Insert(linkedList<int> myLList, int n)
     {
+        int i = 0;
         LinkedListNode<int> current = myLList.First;
-        LinkedListNode<int> result = null;
-
-        if (current.Value >= n)
-        {
-            result = myLList.AddFirst(n);
-            return result;
-        }
-
+        LinkedListNode<int> new = new LinkedListNode<int>;
+        new.Value = n;
         while (current != null)
         {
-            if (current.Next == null || current.Next.Value > n)
+            if (current.Value < n)
             {
-                result = myLList.AddAfter(current, n);
-                break;
+                if (current.Prev == null)
+                {
+                    
+                }
             }
+            i++;
             current = current.Next;
-
         }
-        return result;
     }
 }
