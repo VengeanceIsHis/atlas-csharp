91059236c4d193b546098dc04c0b20e76f25a211
VengeanceIsHis <elijahjpoole@gmail.com>
2024-09-15 15:53:03 -0500

    Test

---
+++ csharp-generics/2-dequeue/queue.cs
@@ -0,0 +1,54 @@
+using System;
+
+
+public class Queue<T>
+{
+    public Node head { get; private set; }
+    public Node tail { get; private set; }
+    public int count { get; private set; }
+
+    public Queue()
+    {
+        head = null;
+        tail = null;
+        count = 0;
+    }
+
+    public class Node
+    {
+        public T value = default(T);
+        public Node next = null;
+
+        public Node(T value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+    }
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+    
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node(value);
+        if (tail != null)
+        {
+            tail.next = newNode;
+        }
+        tail = newNode;
+
+        if (head == null)
+        {
+            head = newNode;
+        }
+        count++;
+    }
+
+    public int Count()
+    {
+        return this.count;
+    }
+
+}
