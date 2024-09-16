5a9f97c1aa14ce2bba6ce1c34cd8110f9f62256d
VengeanceIsHis <elijahjpoole@gmail.com>
2024-09-16 13:04:12 -0500

    Adding task 3

---
+++ csharp-delegates_events/3-modified_behavior/modifier.cs
@@ -0,0 +1,105 @@
+using System;
+
+
+public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    public Player(string name="Player", float maxHp=100f)
+    {
+        if (maxHp < 0)
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        else
+        {
+            this.maxHp = maxHp;
+        }
+        this.name = name;
+
+        this.hp = this.maxHp;
+    }
+
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+    }
+
+    public delegate void CalculateHealth(float number);
+
+    public void TakeDamage(float damage)
+    {
+        
+        if (damage < 0)
+        {
+            Console.WriteLine($"{this.name} takes 0 damage!");
+        }
+        else
+        {
+
+            float newHp = this.hp;
+            newHp -= damage;
+            ValidateHP(newHp);
+            Console.WriteLine($"{this.name} takes {damage} damage!");
+        }
+    }
+
+    public void HealDamage(float heal)
+    {
+        
+        if (heal < 0)
+        {
+            Console.WriteLine($"{this.name} heals 0 HP!");
+        }
+        else
+        {
+            float newHp = this.hp;
+            newHp += heal;
+            ValidateHP(newHp);
+            Console.WriteLine($"{this.name} heals {heal} HP!");
+        }
+    }
+    
+    public void ValidateHP(float newHp)
+    {
+        if (newHp < 0)
+        {
+            this.hp = 0;
+        }
+        else if (newHp > maxHp)
+        {
+            this.hp = this.maxHp;
+        }
+        else
+        {
+            this.hp = newHp;
+        }
+    }
+}
+
+
+
+public enum Modifier
+{
+    Weak,
+    Base,
+    Strong
+}
+public delegate float Calculate Modifier(float baseBalue, Modifier modifier);
+
+
+public float ApplyModifier(float baseValue, Modifier modifier)
+{
+    switch (modifier)
+    {
+        case Modifier.Weak:
+            return baseValue * .5f;
+        case Modifier.Base:
+            return baseValue;
+        case Modifier.Strong:
+            return baseValue * 1.5f;
+    }
+}
