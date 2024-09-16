145ed8ea10bc04d113745c02fc8f3d815f02f4cb
VengeanceIsHis <elijahjpoole@gmail.com>
2024-09-16 15:03:40 -0500

    removing unncessary code

--- csharp-delegates_events/4-check_yourself/check.cs
+++ csharp-delegates_events/4-check_yourself/check.cs
@@ -22,7 +22,7 @@ public class Player
             }
         }
     }
-    public Player(float newHp, string name="Player", float maxHp=100f)
+    public Player(string name="Player", float maxHp=100f)
     {
         if (maxHp < 0)
         {
