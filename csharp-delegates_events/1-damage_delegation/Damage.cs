df1106acd8233785c1a93a5fae8e729200b0d58a
VengeanceIsHis <elijahjpoole@gmail.com>
2024-09-16 12:10:21 -0500

    Test for file name

---
+++ csharp-delegates_events/1-damage_delegation/Damage.cs
@@ -0,0 +1,30 @@
+susing System;
+
+
+public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    public Player(string name="Player", float maxHp=100f)
+    {
+        if (maxHp < 0)
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        else
+        {
+            this.maxHp = maxHp;
+        }
+        this.name = name;
+        
+        this.hp = this.maxHp;
+    }
+
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+    }
+}
