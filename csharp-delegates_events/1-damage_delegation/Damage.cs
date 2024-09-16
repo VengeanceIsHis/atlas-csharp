446b3722a2a50ec32a166367999662a6640afb13
VengeanceIsHis <elijahjpoole@gmail.com>
2024-09-16 12:24:32 -0500

    True test for task 1

--- csharp-delegates_events/1-damage_delegation/Damage.cs
+++ csharp-delegates_events/1-damage_delegation/Damage.cs
@@ -19,12 +19,32 @@ public class Player
             this.maxHp = maxHp;
         }
         this.name = name;
-        
+
         this.hp = this.maxHp;
     }
 
     public void PrintHealth()
     {
         Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+    }
+
+    public delegate void CalculateHealth(float number);
+
+    public void TakeDamage(float damage)
+    {
+        Console.WriteLine($"{this.name} takes {damage}!");
+        if (damage < 0)
+        {
+            Console.WriteLine($"{this.name} takes 0 damage!");
+        }
+    }
+
+    public void HealDamage(float heal)
+    {
+        Console.WriteLine($"{this.name} heals {heal} HP!");
+        if (heal < 0)
+        {
+            Console.WriteLine($"{this.name} heals 0 HP!");
+        }
     }
 }
