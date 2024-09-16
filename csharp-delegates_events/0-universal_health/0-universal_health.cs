88dc173c00294dba6b8107475e5ef75ec541751b
VengeanceIsHis <elijahjpoole@gmail.com>
2024-09-16 12:03:43 -0500

    Tetst

--- csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -3,9 +3,9 @@ using System;
 
 public class Player
 {
-    public string name { get; set; }
-    public float maxHp { get; set; }
-    public float hp { get; set; }
+    public string name { get; private set; }
+    public float maxHp { get; private set; }
+    public float hp { get; private set; }
 
     public Player(string name="Player", float maxHp=100f)
     {
