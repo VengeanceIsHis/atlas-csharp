c152f5d903a22429f4e1a5e034599a1852ce5d8c
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-09-19 12:53:33 -0500

    First Unit Test

---
+++ csharp-text_based_interface/InventoryLibrary/InventoryManagement.Tests/InventoryTest.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using InventoryLibrary;
+
+
+namespace InventoryManagement.Tests;
+
+public class Tests
+{
+    private User _user;
+    private Item _item;
+    private Inventory _inventory;
+
+    [SetUp]
+    public void Setup()
+    {
+        _user = new User("Charlie Day");
+        _item = new Item("556");
+        _inventory = new Inventory(_user.id, _item.id)
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Equal("Charlie Day", _user.name);
+    }
+}
