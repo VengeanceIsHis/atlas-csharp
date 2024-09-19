2b8e991312e6600d6a058c82aadc31d660095e03
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-09-19 12:00:05 -0500

    Big test

---
+++ csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,8 @@
+﻿namespace InventoryLibrary;
+
+public class BaseClass
+{
+    public string id { get; private set }
+    public DateTime date_created { get; private set }
+    public DateTime date_updatede { get; private set }
+}
